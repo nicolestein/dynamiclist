93f6d7e1c32256ebbca96e4adf4bb44e63236ffc
Nicole I Stein <ns60549@srcs.loc>
2018-04-02 11:26:00 -0700

    work

---
+++ dom_nodes.js
@@ -0,0 +1,11 @@
+var submitbutton = document.getElementById("submit");
+submitbutton.addEventListener("click", () => additem(););
+var list = document.getElementsByTagName("ul")[0];
+    function additem() {
+        var imput = document.getElementById("item").value;
+        var li = document.createElement("li");
+        var textcontent = document.createElement("empty");
+        var deletebutton = document.createElement("button");
+
+
+    }
